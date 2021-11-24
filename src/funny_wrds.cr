b1d03701fce9cb3dd50135cccd0f62144e2b43f1
UberNihilist <dx98171@gmail.com>
2021-11-24 21:54:52 +0200

    some changes

--- src/funny_wrds.cr
+++ src/funny_wrds.cr
@@ -1,19 +1,21 @@
 require "json"
 
-class FunnyWords
-	@words                      : JSON::Any
-    property path_to_words      : String
+module FW 
+    VERSION = "0.1.1"
+    class FunnyWords
+	    @words                      : JSON::Any
+        property path_to_words      : String
 
-    def initialize(@path_to_words)
-    	@words = File.open(@path_to_words) { |file| JSON.parse(file) }
-	end
+        def initialize(@path_to_words)
+    	    @words = File.open(@path_to_words) { |file| JSON.parse(file) }
+	    end
 
-    def get_words(n = 1)
-        @words.as_a.sample(n)
+        def get_words(n = 1)
+            @words.as_a.sample(n)
+        end
+
+        def get_concatenated_words()
+            @words.as_a.sample + @words.as_a.sample
+        end
     end
-
-    def get_concatenated_words()
-        @words.as_a.sample + @words.as_a.sample
-    end
-
 end
