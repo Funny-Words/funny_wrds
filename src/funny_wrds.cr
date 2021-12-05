add109926f2036926818d18e6fe70da1228c8701
UberNihilist <dx98171@gmail.com>
2021-12-05 22:10:40 +0200

    wtf

--- src/funny_wrds.cr
+++ src/funny_wrds.cr
@@ -1,18 +1,18 @@
 require "json"
 
 class FunnyWords
-    @file : JSON::Any
+  @file = 0
 
-    def open_file(path_to_file)
-        @file = File.open(path_to_file) { |file| JSON.parse(file) }
-        self
-    end
+  def open_file(path_to_file)
+    @file = File.open(path_to_file) { |file| JSON.parse(file) }
+    self
+  end
 
-    def get_words(n = 1)
-        @file.as_a.sample(n)
-    end
+  def get_words(n = 1)
+    @file.as_a.sample(n)
+  end
 
-    def get_concatenated_words()
-        "#{@file.as_a.sample}#{@file.as_a.sample}"
-    end
+  def get_concatenated_words
+    "#{@file.as_a.sample}#{@file.as_a.sample}"
+  end
 end
